ce5d23dd1f66c7990bb16c700395671d454d9034
Dobauron <safetysafetystrazak@gmail.com>
2023-08-14 18:02:01 +0200

    data printed

--- first/data_test.py
+++ first/data_test.py
@@ -5,13 +5,22 @@ IRIS_TRAINING = 'iris_training.csv'
 # dataset = tf.data.experimental.CsvDataset(filenames=IRIS_TRAINING,
 #                                           record_defaults=[tf.float32],)
 feature_types = [tf.string, tf.string, tf.int32, tf.int32]  # Example feature types
-default_values = [1.011241,2.02,3.03]  # Example default values
+default_values = [1.011241, 2.02, 3.03]  # Example default values
 
 column_defaults = default_values * 2
+record_defaults = [tf.constant(0, dtype=tf.int32),         # Column 1 (int)
+                   tf.constant(0.0, dtype=tf.float32),      # Column 2 (float)
+                   tf.constant(0.0, dtype=tf.float32),      # Column 3 (float)
+                   tf.constant(0.0, dtype=tf.float32),      # Column 3 (float)
+                   tf.constant(0.0, dtype=tf.float32)]
 
+select_cols = [0,1,2,3]
 # Create a dataset from the CSV file
-dataset = tf.data.experimental.CsvDataset(IRIS_TRAINING, default_values, header=True)
+dataset = tf.data.experimental.CsvDataset(IRIS_TRAINING, record_defaults, header= True)
 
 # Iterate over the dataset
-for record in dataset:
-    print(record)  # Process the record as per your requirements
+print(dataset)
+print(type(dataset))
+
+for el in dataset.as_numpy_iterator():
+    print(el)
